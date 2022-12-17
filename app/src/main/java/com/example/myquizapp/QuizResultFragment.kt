c28f22e07dae5e81263e350c94f6138e16f5b736
Alex Green <greenalex778@gmail.com>
2022-12-17 22:02:01 +0300

    1 lesson UI

---
+++ app/src/main/java/com/example/myquizapp/QuizResultFragment.kt
@@ -0,0 +1,28 @@
+package com.example.myquizapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+
+class QuizResultFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        }
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_quiz_result, container, false)
+    }
+
+
+}
